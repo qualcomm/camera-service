b6a1245d6f6bc56c6edb0624d1e54150325a35f3
skomakul <skomakul@qti.qualcomm.com>
2026-02-09 11:07:00 +0530

    cam-server: adding copyright
    as per opensource compliance adding the copyright.
    
    Change-Id: I861e8f649ccea1016e4afc8676ac72fb2ea4085b

--- parse_compilation.py
+++ parse_compilation.py
@@ -1,3 +1,7 @@
+#  Changes from Qualcomm Technologies, Inc. are provided under the following license:
+#  Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+#  SPDX-License-Identifier: BSD-3-Clause-Clear
+
 from __future__ import print_function
 
 import argparse
